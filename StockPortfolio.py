5e3d1c02b9d0859e4b2dcf52d1fc48415db9741a
Harchini Palanisamy <harchupalanisamy32006@gmail.com>
2026-06-01 18:45:23 +0530

    Added Stock Portfolio Tracker Project

---
+++ StockPortfolio.py
@@ -0,0 +1,30 @@
+stock_prices = {
+    "AAPL": 180,
+    "TSLA": 250,
+    "GOOG": 200,
+    "MSFT": 300
+}
+
+total_investment = 0
+
+num_stocks = int(input("How many stocks do you want to enter? "))
+
+for i in range(num_stocks):
+
+    stock_name = input("Enter stock name: ").upper()
+
+    quantity = int(input("Enter quantity: "))
+
+    
+    if stock_name in stock_prices:
+
+        price = stock_prices[stock_name]
+        investment = price * quantity
+
+        total_investment += investment
+
+        print("Investment for", stock_name, "=", investment)
+
+    else:
+        print("Stock not found!")
+print("\nTotal Investment Value =", total_investment)
