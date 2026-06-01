1b184d0aae55bad103d14d45e253fe02de1d9f81
Harchini Palanisamy <harchupalanisamy32006@gmail.com>
2026-06-01 19:05:12 +0530

    Improved portfolio text report

--- StockPortfolio.py
+++ StockPortfolio.py
@@ -1,11 +1,14 @@
 stock_prices = {
-    "AAPL": 180,
-    "TSLA": 250,
-    "GOOG": 200,
-    "MSFT": 300
+    "APPLE": 180,
+    "TESLA": 250,
+    "GOOGLE": 200,
+    "MICROSCOFT": 300
 }
 
 total_investment = 0
+
+file = open("portfolio.txt", "w")
+file.write("Stock Portfolio Report\n\n")
 
 num_stocks = int(input("How many stocks do you want to enter? "))
 
@@ -15,7 +18,6 @@ for i in range(num_stocks):
 
     quantity = int(input("Enter quantity: "))
 
-    
     if stock_name in stock_prices:
 
         price = stock_prices[stock_name]
@@ -25,11 +27,14 @@ for i in range(num_stocks):
 
         print("Investment for", stock_name, "=", investment)
 
+        file.write("Stock: " + stock_name + "\n")
+        file.write("Quantity: " + str(quantity) + "\n")
+        file.write("Investment: " + str(investment) + "\n\n")
+
     else:
         print("Stock not found!")
-print("\nTotal Investment Value =", total_investment)
 
-file = open("portfolio.txt", "w")
+print("\nTotal Investment Value =", total_investment)
 
 file.write("Total Investment Value = " + str(total_investment))
 
